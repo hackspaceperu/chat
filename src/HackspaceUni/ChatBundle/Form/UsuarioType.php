b6e2489b4ed8c547b343b27940edf4ff46deedae
Carlos Salvatierra <cslucano@gmail.com>
2014-02-01 13:57:09 -0500

    agregamos crud para usuario

---
+++ src/HackspaceUni/ChatBundle/Form/UsuarioType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace HackspaceUni\ChatBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class UsuarioType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nombreusuario')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'HackspaceUni\ChatBundle\Entity\Usuario'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'hackspaceuni_chatbundle_usuario';
+    }
+}
